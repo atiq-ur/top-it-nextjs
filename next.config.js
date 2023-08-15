35073aabe9430f09e2dc4bfc5f93eebad21b65d7
Atiqur Rahman <atiq.misterrahman@gmail.com>
2023-08-16 00:14:12 +0600

    fix: configure image loading

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-        
-    ],
-  },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'i.ibb.co',
+            port: '',
+            pathname: '',
+          },
+        ],
+      },
 }
 
 module.exports = nextConfig
